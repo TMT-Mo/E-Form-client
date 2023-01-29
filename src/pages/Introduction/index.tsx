a25c029ae0a0327375d282b23bae17e6e254f040
Mo <113235471+TMT-Mo@users.noreply.github.com>
2023-01-29 17:27:10 +0700

    add phone number and gmail to introduction page

--- src/pages/Introduction/index.tsx
+++ src/pages/Introduction/index.tsx
@@ -155,9 +155,9 @@ const Introduction = () => {
                 >
                   <path d="M17.707 12.293a.999.999 0 0 0-1.414 0l-1.594 1.594c-.739-.22-2.118-.72-2.992-1.594s-1.374-2.253-1.594-2.992l1.594-1.594a.999.999 0 0 0 0-1.414l-4-4a.999.999 0 0 0-1.414 0L3.581 5.005c-.38.38-.594.902-.586 1.435.023 1.424.4 6.37 4.298 10.268s8.844 4.274 10.269 4.298h.028c.528 0 1.027-.208 1.405-.586l2.712-2.712a.999.999 0 0 0 0-1.414l-4-4.001zm-.127 6.712c-1.248-.021-5.518-.356-8.873-3.712-3.366-3.366-3.692-7.651-3.712-8.874L7 4.414 9.586 7 8.293 8.293a1 1 0 0 0-.272.912c.024.115.611 2.842 2.271 4.502s4.387 2.247 4.502 2.271a.991.991 0 0 0 .912-.271L17 14.414 19.586 17l-2.006 2.005z"></path>
                 </svg>
-                <span className="hover:font-semibold cursor-pointer">
-                  0123456789
-                </span>
+                <a className="hover:font-semibold text-white no-underline" href="tel:+84775509168">
+                  0775509168
+                </a>
               </div>
               <div className="flex space-x-4 items-center">
                 <svg
@@ -168,9 +168,9 @@ const Introduction = () => {
                 >
                   <path d="M20 4H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm0 2v.511l-8 6.223-8-6.222V6h16zM4 18V9.044l7.386 5.745a.994.994 0 0 0 1.228 0L20 9.044 20.002 18H4z"></path>
                 </svg>
-                <span className="hover:font-semibold cursor-pointer">
-                  abc@gmail.com
-                </span>
+                <a  className="hover:font-semibold text-white no-underline" href="https://mail.google.com/mail/?view=cm&fs=1&to=minhtriet17101@gmail.com" target="_blank" rel="noreferrer">
+                  minhtriet17101@gmail.com
+                </a>
               </div>
               <div className="flex space-x-4 items-center">
                 <svg
